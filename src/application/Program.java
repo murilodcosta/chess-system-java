b983343367b4a7bb2502969360d3b5757f3d1795
murilo.costa <murilodiasdacosta@gmail.com>
2024-10-15 23:08:20 -0300

    Printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,14 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessMatch;
+
 public class Program {
     public static void main (String [] args){
-        System.out.println("Hello World");
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
